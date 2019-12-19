5854b97d0a1fa13fc688415ff56c2075d9cd6227
Andriy Chernikov <chernikov@gmail.com>
2019-12-19 12:07:21 +0200

    [hotfix] Fix serialization to camelCase

--- avianoise.Web/Services/IdentityService.cs
+++ avianoise.Web/Services/IdentityService.cs
@@ -1,10 +1,12 @@
 ﻿using AutoMapper;
 using avianoise.BL;
+using avianoise.Domain;
 using avianoise.Web.Dto;
 using avianoise.Web.Helpers;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
@@ -17,18 +19,16 @@ namespace avianoise.Web.Services
 {
     public class IdentityService : IIdentityService
     {
-        private readonly IPrincipal principal;
         private readonly AppSettings appSettings;
         private readonly IMapper mapper;
         private readonly IUserAuthBL userAuthBL;
-        private readonly IUserBL userBL;
 
         public IdentityService(IOptions<AppSettings> appSettings, IMapper mapper, IUserAuthBL userAuthBL)
         {
             this.appSettings = appSettings.Value;
             this.mapper = mapper;
             this.userAuthBL = userAuthBL;
-            userBL = userBL;
+
         }
 
 
@@ -41,11 +41,21 @@ namespace avianoise.Web.Services
                 return null;
             }
 
+            return CreateToken(user);
+        }
+
+        private string CreateToken(User user)
+        {
             // authentication successful so generate jwt token
             var tokenHandler = new JwtSecurityTokenHandler();
             var key = Encoding.ASCII.GetBytes(appSettings.Secret);
 
-            var objUser = JsonConvert.SerializeObject(mapper.Map<UserDto>(user));
+            var serializerSettings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
+
+            var objUser = JsonConvert.SerializeObject(mapper.Map<UserDto>(user), serializerSettings);
             var claims = new List<Claim>() {
                             new Claim(ClaimTypes.Sid, user.Id.ToString()),
                             new Claim(ClaimTypes.Name, user.Email),
@@ -76,32 +86,7 @@ namespace avianoise.Web.Services
             {
                 return null;
             }
-
-            // authentication successful so generate jwt token
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
-
-            var objUser = JsonConvert.SerializeObject(mapper.Map<UserDto>(user));
-            var claims = new List<Claim>() {
-                            new Claim(ClaimTypes.Sid, user.Id.ToString()),
-                            new Claim(ClaimTypes.Name, user.Email),
-                            new Claim("user", objUser)
-            };
-            var roles = user.UserRoles.Select(ur => ur.Role);
-            foreach (var role in roles)
-            {
-                claims.Add(new Claim(ClaimTypes.Role, role.Code));
-            }
-
-            var identity = new ClaimsIdentity(claims);
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = identity,
-                Expires = DateTime.UtcNow.AddDays(7),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-            };
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-            return tokenHandler.WriteToken(token);
+            return CreateToken(user);
         }
     }
 }
