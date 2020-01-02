f16b8f3c178bc20aa0368aaf3c21021b0760b8ea
Andrey Chernikov <chernikov@gmail.com>
2020-01-02 12:03:38 +0200

    [fix] staging as product

--- avianoise.Web/Startup.cs
+++ avianoise.Web/Startup.cs
@@ -92,12 +92,8 @@ namespace avianoise.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseDeveloperExceptionPage();
             app.UseRequestLog();
-
 
             app.UseStaticFiles(
                new StaticFileOptions
