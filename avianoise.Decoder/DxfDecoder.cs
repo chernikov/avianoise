c77ac330ed349edb6668fc476407bd5af49b6206
Andrey Chernikov <chernikov@gmail.com>
2020-01-14 10:15:37 +0200

    [fix] #26 Decode new dxf file

--- avianoise.Decoder/DxfDecoder.cs
+++ avianoise.Decoder/DxfDecoder.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using IxMilia.Dxf;
+using IxMilia.Dxf.Entities;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -8,40 +11,80 @@ namespace avianoise.Decoder
 {
     public class DxfDecoder : IDecoder
     {
-        private static readonly Regex PolylineRegex = new Regex("POLYLINE(?<polyline>.*?)SEQEND", RegexOptions.Singleline);
-
         public List<Line> Decode(string content)
         {
             var list = new List<Line>();
-            var matches = PolylineRegex.Matches(content);
-            foreach (Match match in matches)
+            DxfFile dxfFile;
+            using (var fs = GenerateStreamFromString(content))
             {
-                var group = match.Groups[1];
+                dxfFile = DxfFile.Load(fs);
+            }
 
-                var polyline = group.Value;
-                var splitByLine = polyline.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
-                var line = new Line
+            foreach (var entity in dxfFile.Entities)
+            {
+                switch (entity.EntityType)
                 {
-                    Name = splitByLine[1]
-                };
-
-                var points = Regex.Split(polyline, "VERTEX", RegexOptions.Singleline).ToList();
-                points = points.Skip(1).ToList();
-                var index = 1;
-                foreach (var point in points)
-                {
-                    var pointsPart = point.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
-                    line.Points.Add(new Point()
-                    {
-                        Index = index,
-                        Lat = Convert.ToDouble(pointsPart[5], CultureInfo.InvariantCulture),
-                        Lng = Convert.ToDouble(pointsPart[3], CultureInfo.InvariantCulture)
-                    });
-                    index++;
+                    case DxfEntityType.LwPolyline:
+                        list.Add(GetLwPolyline(entity));
+                        break;
+                    case DxfEntityType.Polyline:
+                        list.Add(GetPolyline(entity));
+                        break;
                 }
-                list.Add(line);
             }
             return list;
+        }
+        private Line GetPolyline(DxfEntity entity)
+        {
+            var polyline = entity as DxfPolyline;
+            var line = new Line()
+            {
+                Name = polyline.Layer
+            };
+            var index = 1;
+            foreach (var vertex in polyline.Vertices)
+            {
+                line.Points.Add(new Point()
+                {
+                    Index = index,
+                    Lat = vertex.Location.Y,
+                    Lng = vertex.Location.X
+                });
+                index++;
+            }
+            return line;
+        }
+
+
+        private Line GetLwPolyline(DxfEntity entity)
+        {
+            var polyline = entity as DxfLwPolyline;
+            var line = new Line()
+            {
+                Name = polyline.Layer
+            };
+            var index = 1;
+            foreach (var vertex in polyline.Vertices)
+            {
+                line.Points.Add(new Point()
+                {
+                    Index = index,
+                    Lat = vertex.Y,
+                    Lng = vertex.X
+                });
+                index++;
+            }
+            return line;
+        }
+
+        private Stream GenerateStreamFromString(string s)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
         }
     }
 }
