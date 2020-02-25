5f6aa1c1577cd1fa7701e1b5d952c2bf376234c2
Andrey Chernikov <chernikov@gmail.com>
2020-02-25 16:33:20 +0200

    [feature] Add Quill Text editor

---
+++ avianoise.Client/src/app/modules/admin/_components/edit-post/edit-post.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-post',
+  templateUrl: './edit-post.component.html',
+  styleUrls: ['./edit-post.component.scss']
+})
+export class EditPostComponent implements OnInit {
+
+  content : string = "";
+  editorOptions = {
+    toolbar: [
+      ['bold', 'italic', 'underline', 'strike'],
+      [{ 'header': 1 }, { 'header': 2 }],
+      [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+      [{ 'script': 'sub' }, { 'script': 'super' }],
+      [{ 'indent': '-1' }, { 'indent': '+1' }],
+      [{ 'size': ['small', false, 'large', 'huge'] }],
+      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+      [{ 'color': [] }, { 'background': [] }],
+      [{ 'align': [] }],
+      ['link'],
+      ['image']
+    ],
+  };
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+}
