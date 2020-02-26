eea1fce18dab9c3bec071ab1c192edbcaebf7ffd
Andrey Chernikov <chernikov@gmail.com>
2020-02-26 12:08:50 +0200

    [remove] Add Migration

---
+++ avianoise.Data/Migrations/20200226100811_RemovePostFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace avianoise.Data.Migrations
+{
+    public partial class RemovePostFile : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PostFiles");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PostFiles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AddedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FullPath = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PostFiles", x => x.Id);
+                });
+        }
+    }
+}
